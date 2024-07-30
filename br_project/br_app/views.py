7561ed7e8cb5098ab8a1a646c556c73057bb2247
gregothedeveloper <gregoforty2@gmail.com>
2024-07-31 02:16:01 +0300

    First Project Done

--- br_project/br_app/views.py
+++ br_project/br_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return response
+    return HttpResponse('Hoorey!!, This is my first project!')
